d8ace67385d94f1bc3661f12df12cb3f6c254f04
OurUmbracoPackages <54137413+OurUmbracoPackages@users.noreply.github.com>
2019-08-15 15:00:52 +0200

    Update controller.js

--- App_Plugins/controller.js
+++ App_Plugins/controller.js
@@ -7,9 +7,9 @@ function ($scope,assetsService) {
 	// plugin folder
 	assetsService
 		.load([
-			"~/App_Plugins/MarkDownEditor/lib/markdown.converter.js",
-			"~/App_Plugins/MarkDownEditor/lib/markdown.sanitizer.js",
-			"~/App_Plugins/MarkDownEditor/lib/markdown.editor.js"
+			"~/App_Plugins/{{repository_name}}/lib/markdown.converter.js",
+			"~/App_Plugins/{{repository_name}}/lib/markdown.sanitizer.js",
+			"~/App_Plugins/{{repository_name}}/lib/markdown.editor.js"
 		])
 		.then(function () {
 			// this function will execute when all dependencies have loaded
@@ -17,7 +17,7 @@ function ($scope,assetsService) {
 		});
 
 	// load the separate css for the editor to avoid it blocking our JavaScript loading
-	assetsService.loadCss("~/App_Plugins/MarkDownEditor/lib/markdown.editor.less");
+	assetsService.loadCss("~/App_Plugins/{{repository_name}}/lib/markdown.editor.less");
   
   var converter2 = new Markdown.Converter();
   var editor2 = new Markdown.Editor(converter2, "-" + $scope.model.alias);
